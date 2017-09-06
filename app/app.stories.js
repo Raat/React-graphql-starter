d0de827774a12ff998a63f830158de9ec029621a
jeffrey raat <jeffreyraat@gmail.com>
2017-09-06 22:54:05 +0200

    add styleguidist for docs

--- app/app.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './app';
-import test from './app.tests';
-
-storiesOf('App', module).add('default', () => {
-  const story = <App />;
-  specs(test);
-  return story;
-});
