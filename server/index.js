a9624935821f3d8ee219e3a23ce3f1a762c9c996
jeffrey raat <jeffreyraat@gmail.com>
2017-10-18 21:47:16 +0200

    update babel-register

--- server/index.js
+++ server/index.js
@@ -1,2 +1,2 @@
-require('babel-register'); // register babel so we can use fancy es6/7
+require('babel-core/register'); // register babel so we can use fancy es6/7
 require('./server'); // lets start
